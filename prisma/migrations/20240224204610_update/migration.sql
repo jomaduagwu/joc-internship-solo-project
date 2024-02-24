4e23b4a341933bbf45216b124fb69504528367cd
Ms Eko <jomaduagwu@gmail.com>
2024-02-24 14:46:47 -0600

    updated schema

--- prisma/migrations/20240224204610_update/migration.sql
+++ prisma/migrations/20240224204610_update/migration.sql
@@ -5,6 +5,7 @@ CREATE TABLE `Task` (
     `description` VARCHAR(191) NOT NULL,
     `priority` ENUM('LOW', 'MEDIUM', 'HIGH') NOT NULL,
     `dueDate` DATETIME(3) NOT NULL,
+    `status` ENUM('OPEN', 'IN_PROGRESS', 'CLOSED') NOT NULL DEFAULT 'OPEN',
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
     `complete` BOOLEAN NOT NULL,
