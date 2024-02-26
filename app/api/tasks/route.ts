bba206cccbe018c4f3707db2e8fe279a34a4071b
Ms Eko <jomaduagwu@gmail.com>
2024-02-26 16:21:36 -0600

    updated API for creating issues

---
+++ app/api/tasks/route.ts
@@ -0,0 +1,42 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+import { taskSchema } from "@/app/schema";
+
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+ 
+  const validation = taskSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(
+      { error: "Validation failed", details: validation.error.errors },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const newTask = await prisma.task.create({
+      data: {
+        title: body.title,
+        description: body.description,
+        dueDate: body.dueDate,
+        priority: body.priority,
+        status: 'OPEN',
+        complete: false, 
+      },
+    });
+  
+ 
+    return NextResponse.json(newTask, { status: 201 });
+  } catch (error: any) {
+    console.error("Error creating task:", error);
+  
+
+    return NextResponse.json(
+      { error: "Internal Server Error", details: error.message },
+      { status: 500 }
+    );
+  }
+}
