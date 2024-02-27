c76971b7db0905bb8bc96bcf6b9f4e2cc1b9c0de
Ms Eko <jomaduagwu@gmail.com>
2024-02-26 19:46:21 -0600

    updated theme and new page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
+import "@radix-ui/themes/styles.css";
+import "./theme-config.css"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./NavBar";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter", });
 
 export const metadata: Metadata = {
   title: "Task Manager App",
@@ -16,8 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-cyan-950 text-slate-100 container mx-auto p-4"><NavBar />
-      <main>{children}</main>
+      <body className={inter.variable}>
+        {/* <body className="bg-cyan-950 text-slate-100 container mx-auto p-4"> */}
+        <Theme appearance="light" accentColor="jade">
+          <NavBar />
+          <main className="p-5">{children}</main>
+        </Theme>
       </body>
     </html>
   );
